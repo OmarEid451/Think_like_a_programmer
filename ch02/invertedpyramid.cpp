064e3704171ad4a07c00a6a9f3feb172ef2f7bae
OmarEid451 <Omar_dev_999@proton.me>
2024-05-19 11:26:22 -0700

    changed inverted pyramind to better reflect inequality graph

--- ch02/invertedpyramid.cpp
+++ ch02/invertedpyramid.cpp
@@ -7,10 +7,10 @@ int main() {
     int lower_bound = i;
     int upper_bound = max_line_length - i;
     for (int k = 0; k < max_line_length; ++k) {
-      if (k < lower_bound || k >= upper_bound) {
-        cout << ' ';
-      } else {
+      if (k >= lower_bound && k < upper_bound) {
         cout << '#';
+      } else {
+        cout << ' ';
       }
     }
     cout << '\n';

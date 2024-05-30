a63b2d2f21cfdb44a78a27c6c51de3d1e2b4b66b
OmarEid451 <Omar_dev_999@proton.me>
2024-05-29 23:06:22 -0700

    revised plan for 2-3

--- ch02/exercise2-3.cpp
+++ ch02/exercise2-3.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 // Exercise 2-3 from think like a programmer
-// code is the same as in 2-2 we just change use a union inequality instead of an intersection 
+// code is the same as in 2-2 we just change use a union inequality instead of
+// an intersection we need to write code that will not only shift the range but
+// also the starting point for each range maybe pass the starting line in as
+// a variable?
 
 
 const int max_line_length = 8;
@@ -17,6 +20,8 @@ void draw_line(int upper_bound, int lower_bound) {
   }
   cout << '\n';
 }
+
+// TODO create inverted line function that draws the middle lines.
 
 int main() {
   int lower_bound, upper_bound;
